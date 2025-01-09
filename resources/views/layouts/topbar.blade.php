8df1d25cd13688b418a3c210858277d07132eaac
Sal Aime <salemnk02@gmail.com>
2025-01-09 10:39:44 +0100

    update lang, favicon and logo

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -42,27 +42,18 @@
                             class="inline-flex justify-center items-center p-0 text-topbar-item transition-all w-[37.5px] h-[37.5px] duration-200 ease-linear bg-topbar rounded-md dropdown-toggle btn hover:bg-topbar-item-bg-hover hover:text-topbar-item-hover group-data-[topbar=dark]:bg-topbar-dark group-data-[topbar=dark]:hover:bg-topbar-item-bg-hover-dark group-data-[topbar=dark]:hover:text-topbar-item-hover-dark group-data-[topbar=brand]:bg-topbar-brand group-data-[topbar=brand]:hover:bg-topbar-item-bg-hover-brand group-data-[topbar=brand]:hover:text-topbar-item-hover-brand group-data-[topbar=dark]:dark:bg-zink-700 group-data-[topbar=dark]:dark:hover:bg-zink-600 group-data-[topbar=dark]:dark:text-zink-500 group-data-[topbar=dark]:dark:hover:text-zink-50"
                             id="flagsDropdown" data-bs-toggle="dropdown">
                             @switch(Session::get('lang'))
-                                @case('fr')
-                                    <img src="{{ URL::asset('build/images/flags/20/fr.svg') }}" alt=""
+                                @case('en')
+                                    <img src="{{ URL::asset('build/images/flags/20/us.svg') }}" alt=""
                                         id="header-lang-img" class="h-5 rounded-sm">
                                 @break
 
                                 @default
-                                    <img src="{{ URL::asset('build/images/flags/20/us.svg') }}" alt=""
+                                    <img src="{{ URL::asset('build/images/flags/20/fr.svg') }}" alt=""
                                         id="header-lang-img" class="h-5 rounded-sm">
                             @endswitch
                         </button>
                         <div class="absolute z-50 hidden p-4 ltr:text-left rtl:text-right bg-white rounded-md shadow-md !top-4 dropdown-menu min-w-[10rem] flex flex-col gap-4 dark:bg-zink-600"
                             aria-labelledby="flagsDropdown">
-                            <a href="{{ url('index/en') }}" class="flex items-center gap-3 group/items language"
-                                data-lang="en" title="English">
-                                <img src="{{ URL::asset('build/images/flags/20/us.svg') }}" alt=""
-                                    class="object-cover h-4 rounded-full">
-                                <h6
-                                    class="transition-all duration-200 ease-linear font-15medium text- text-slate-600 dark:text-zink-200 group-hover/items:text-custom-500">
-                                    English</h6>
-                            </a>
-
                             <a href="{{ url('index/fr') }}" class="flex items-center gap-3 group/items language"
                                 data-lang="fr" title="French">
                                 <img src="{{ URL::asset('build/images/flags/20/fr.svg') }}" alt=""
@@ -70,6 +61,15 @@
                                 <h6
                                     class="transition-all duration-200 ease-linear font-15medium text- text-slate-600 dark:text-zink-200 group-hover/items:text-custom-500">
                                     French</h6>
+                            </a>
+
+                            <a href="{{ url('index/en') }}" class="flex items-center gap-3 group/items language"
+                                data-lang="en" title="English">
+                                <img src="{{ URL::asset('build/images/flags/20/us.svg') }}" alt=""
+                                    class="object-cover h-4 rounded-full">
+                                <h6
+                                    class="transition-all duration-200 ease-linear font-15medium text- text-slate-600 dark:text-zink-200 group-hover/items:text-custom-500">
+                                    English</h6>
                             </a>
                         </div>
                     </div>
